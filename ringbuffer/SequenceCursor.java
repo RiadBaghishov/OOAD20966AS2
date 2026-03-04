4515db1f17439e74397451b51c3e4b035f17bd66
RiadBaghishov <rbaghishov17368@ada.edu.az>
2026-03-04 23:42:15 +0400

    SequenceCursor.java

---
+++ ringbuffer/SequenceCursor.java
@@ -0,0 +1,25 @@
+package com.example.ringbuffer;
+
+/**
+ * Holds a reader's current sequence position (next sequence to read).
+ * Separated as its own class to keep responsibilities clean.
+ */
+final class SequenceCursor {
+    private long nextReadSeq;
+
+    SequenceCursor(long startSeq) {
+        this.nextReadSeq = startSeq;
+    }
+
+    long nextReadSeq() {
+        return nextReadSeq;
+    }
+
+    void setNextReadSeq(long value) {
+        this.nextReadSeq = value;
+    }
+
+    void advance() {
+        this.nextReadSeq++;
+    }
+}
